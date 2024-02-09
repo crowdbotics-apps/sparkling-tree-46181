574540ac57722116900be19f853fd494be61f97a
Crowdbotics <team@crowdbotics.com>
2024-02-09 15:35:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sparkling-tree-46181",
+  url: "https://sparkling-tree-46181.botics.co",
+  api: "https://sparkling-tree-46181.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
